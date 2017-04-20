666cbe366035af2206daf13836d7653206e1a7dc
Jonas Hauß <haussjonas@gmail.com>
2017-04-20 15:01:17 +0200

    Tried to reolve issue with dependencies. Still work in progress to get Spring Boot fixed

--- backend/src/main/java/edu/hsog/flat/backend/Application.java
+++ backend/src/main/java/edu/hsog/flat/backend/Application.java
@@ -1,12 +1,20 @@
 package edu.hsog.flat.backend;
 
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.io.File;
-
+@RestController
 @SpringBootApplication
 public class Application {
+	@RequestMapping("/")
+	public String index() {
+		return "index.html";
+	}
+
 	public static void main(String[] args) {
 		SpringBootApplication.run(Application.class, args);
 	}
