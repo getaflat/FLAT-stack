b74cca24d550998c12df93268bbddb776a460c16
Unknown <josuawbr@googlemail.com>
2017-05-06 14:41:39 +0200

    changed Dashboard, and more responsive

--- frontend/src/main/javascript/pages/User/user.jsx
+++ frontend/src/main/javascript/pages/User/user.jsx
@@ -7,6 +7,12 @@ const defaultProps = {};
 
 let email = null;
 
+/*
+ {this.state.users.map((user) =>
+ <label>Geburtsdatum: {user.email} \
+ </label>)}<br />
+*/
+
 class User extends React.Component {
     constructor(props) {
         super(props);
@@ -31,27 +37,19 @@ class User extends React.Component {
             <div className={styles.wrapper}>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
                 <div className={styles.leftDash}>
-                    <div className={styles.pictureName}>
-                        <img src='http://www.texted.ch/wp-content/uploads/2015/02/Beaty.jpg' alt="Userimage" className={styles.userPicture}/>
-                        {this.state.users.map((user) =>
-                            <h3>test</h3>)}
-                    </div>
+                    <h3>tWiedler</h3>
+                    <hr />
                     <div className={styles.userStats}>
-                        {this.state.users.map((user) =>
-                            <label>Vorname: {user.email} \
-                            </label>)}<br /><hr />
-                        {this.state.users.map((user) =>
-                            <label>Nachname: {user.email} \
-                            </label>)}<br />
-                        {this.state.users.map((user) =>
-                            <label>Vertragsnummer: {user.email} \
-                            </label>)}<br />
-                        {this.state.users.map((user) =>
-                            <label>Email-Adresse: {user.email} \
-                            </label>)}<br />
-                        {this.state.users.map((user) =>
-                            <label>Geburtsdatum: {user.email} \
-                            </label>)}<br />
+                            <label>Vorname: Thomas
+                            </label><br />
+                            <label>Nachname: Wiedler
+                            </label><br />
+                            <label>Vertragsnummer: 123412341234
+                            </label><br />
+                            <label>Email-Adresse: test@gmail.com
+                            </label><br />
+                            <label>Geburtsdatum: 19.11.1994
+                            </label><br />
                     </div>
                     <div className={styles.buttons}>
                         <button className={styles.button}>bearbeiten</button>
@@ -60,24 +58,25 @@ class User extends React.Component {
                 </div>
                 <hr className={styles.invis}/>
                 <div className={styles.rightDash}>
-                    <table className={styles.userTable}>
-                        <tbody>
+
+                    <div className={styles.tgwrap}>
+                    <table className={styles.tg}>
                         <tr>
-                            <th>Name</th>
-                            <th>Zeitraum</th>
-                            <th>Status</th>
-                            <th>Auswahl</th>
+                            <th className={styles.tgyw4l}>Name (Buchung)</th>
+                            <th className={styles.tgyw4l}>Zeitraum</th>
+                            <th className={styles.tgyw4l}>Status</th>
+                            <th className={styles.tgyw4l}>Auswahl</th>
                         </tr>
                         {this.state.users.map((user, index) =>
                             <tr key={index}>
                                 <td>{user.email}</td>
                                 <td>{user.birthdate}</td>
                                 <td>{user.email}</td>
-                                <td><input type="checkbox"/></td>
+                                <td className={styles.check}><input type="checkbox"/></td>
                             </tr>
                         )}
-                        </tbody>
                     </table>
+                    </div>
                     <div className={styles.buttonright}>
                     <button className={styles.button}>stornieren</button>
                     </div>
