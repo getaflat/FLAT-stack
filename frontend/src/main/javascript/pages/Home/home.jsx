9ce01240dedaa4d28f8f6a23235bdc4f8a7662e9
Carmen Schmider <cschmide@stud.hs-offenburg.de>
2017-05-15 05:45:28 +0200

    Changed header, login and home

--- frontend/src/main/javascript/pages/Home/home.jsx
+++ frontend/src/main/javascript/pages/Home/home.jsx
@@ -34,19 +34,27 @@ class Home extends React.Component {
             <div className={styles.wrapper}>
                 <div className={styles.leftStart}>
                     <img className={styles.picture} src={this.state.apartment.images} />
-                    <h3>{this.state.apartment.name}</h3>
+                    <h3>{this.state.apartment.name}Foto aus DB</h3>
                 </div>
                 <div className={styles.rightStart}>
-                    <h2>News</h2><br />
+                    <br />
+                    <h2>Alpenregion Karwendel</h2>
                     <p>
-                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
-                        ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo
-                        dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-                        amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
-                        ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo
-                        dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                        Die Alpenregion Karwendel rund um Mittenwald, Krün und Wallgau bietet Ihnen Abwechslung mit unzähligen
+                        Freizeitmöglichkeiten und ist eine der vielseitigsten Urlaubsregionen in Bayern. Die schönste Aussicht
+                        genießen Sie im neuen Komplex Schönwetterblick. Exclusiv bei uns.
+                        <br />
+                        <br />
+                        Buchbar ab August 2017.
                     </p>
-                    <Link to="/error">This should not work.</Link>
+                    <br />
+
+                    <h2>Neue Website!</h2>
+                    <p>
+                        Wir haben unseren Internetauftritt neu gestaltet. Jetzt ist so einfach wie noch nie auch über
+                        Ihr Smartphone bequem eine Ferienwohung zu buchen.
+                    </p>
+                    <br />
                 </div>
 
             </div>
