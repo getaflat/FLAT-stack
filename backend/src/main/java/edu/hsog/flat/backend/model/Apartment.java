26873b536b25db6b51e6db279f5e10a49cdc6de3
Tobin Ch <T.Choinowski@gmail.com>
2017-05-22 14:27:37 +0200

    Added apartments

--- backend/src/main/java/edu/hsog/flat/backend/model/Apartment.java
+++ backend/src/main/java/edu/hsog/flat/backend/model/Apartment.java
@@ -11,7 +11,7 @@ import java.util.List;
 @NoArgsConstructor
 public class Apartment {
 	@Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    //@GeneratedValue(strategy = GenerationType.AUTO)
 	@NonNull
 	private Long apartmentId;
 
